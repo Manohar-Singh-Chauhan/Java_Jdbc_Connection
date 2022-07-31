461816e61130a3cb104ee3377e420f41b72e81b1
Manohar Singh Chauhan <manoharsinghchauhan1@gmail.com>
2022-07-31 18:24:44 +0530

    Update Main.java

--- Maven-intro/src/main/java/com/example/Main.java
+++ Maven-intro/src/main/java/com/example/Main.java
@@ -7,6 +7,9 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+         int a = 10 / 2;
+        int b = 20 / 4;
+        int c = 60 / 6;
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dummy_db","root","12345678");
         Statement statement = connection.createStatement();
         statement.execute("create table jbdl(id int, name varchar(30))");
